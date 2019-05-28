5f7e21db4c3f7c362c2104ed14fbfd56f9a28f20
Schmolke <51118451+Schmolke@users.noreply.github.com>
2019-05-28 12:53:07 +0200

    Create Response.php

---
+++ src/Response.php
@@ -0,0 +1,36 @@
+<?php
+class httpResponse {
+    var $resp;
+
+    function __construct($resp) {
+        $this->resp = $resp;
+    }
+
+    //extract response code
+    function code() {
+        preg_match('/HTTP\/[10\.]+\s+([0-9]+)/', $this->resp, $code);
+        return $code[1];
+    }
+
+    //extract response body
+    function body() {
+        //body is between two empty lines for "chunked" encoding
+        $chunk = preg_split('/(?=^\r)/m',$this->resp);
+        $body = "";
+        if ($chunk[1]){
+            //extract body from \nsize(body)\n0
+            $body = str_replace("\n[0-9a-fA-F]+", "", $chunk[1]);
+        }
+
+
+
+        return trim($body);
+    }
+
+    function array(){
+        $body = explode('<', trim($this->body()), 2);
+        $body = trim('<'.$body[1]);
+        $body = simplexml_load_string($body);
+        return $body;
+    }
+}
